0acda43633f6180e04c2a0ae33159144cea47819
Rishabh <smartrishabh51@gmail.com>
2024-10-19 13:12:22 +0530

    dataset added

---
+++ init/index.js
@@ -0,0 +1,23 @@
+
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+main()
+.then(()=>{
+     console.log("connceted to db");    
+})
+.catch(err =>{
+     console.log(err);
+     
+});
+async function main(){
+     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+//cleaning and adding our data
+const initDB = async () =>{
+     await Listing.deleteMany({});
+     await Listing.insertMany(initData.data);
+     console.log("data was initialized");  
+};
+initDB();
--- models/listing.js
+++ models/listing.js
@@ -2,22 +2,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title:{
-       type:String,
-       required:true,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: Number,
+
+  // Image is now an object with filename and url fields
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
     },
-    description:{
-      type:String,
+    url: {
+      type: String,
+      default: "https://unsplash.com/photos/an-aerial-view-of-a-building-in-the-water-Jz4-YOEownY",
+      set: (v) => (v === "" ? "https://unsplash.com/photos/an-aerial-view-of-a-building-in-the-water-Jz4-YOEownY" : v),
     },
-    price:Number,
-   
-    image:{
-     type : String,
-     default:"https://unsplash.com/photos/an-aerial-view-of-a-building-in-the-water-Jz4-YOEownY",
-     set:(v)=>v===""? "https://unsplash.com/photos/an-aerial-view-of-a-building-in-the-water-Jz4-YOEownY":v,
-    },
-    location:String,
-    country : String,
+  },
+
+  location: String,
+  country: String,
 });
+
+// module.exports = mongoose.model('Listing', listingSchema);
+
 const Listing = mongoose.model("Listing",listingSchema);
 module.exports = Listing;
