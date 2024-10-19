4e1cd82e6d6ee7720d0e86e5ffcbc97ea026afbe
Rishabh <smartrishabh51@gmail.com>
2024-10-19 13:26:20 +0530

    ejs file create

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 main().then(()=>{
      console.log("connceted to db");    
 })
@@ -16,19 +17,23 @@ app.get("/",(req,res)=>{
      res.send("Hi,I a root");
      
 })
-app.get("/testlisting",async(req,res)=>{
-   let sampleListing = new Listing({
-     title :"My New Villa",
-     description:"By the beach", 
-     price:1000,
-     location:"kushinagar",
-     country:"India",
-   });
-   await sampleListing.save();
-   console.log("sample was saved");
-   res.send("successful testing");
+app.get("/listings",async (req,res)=>{
+     const allListings = await Listing.find({});
+     res.render("index.js",{allListings});
+});
+// app.get("/testlisting",async(req,res)=>{
+//    let sampleListing = new Listing({
+//      title :"My New Villa",
+//      description:"By the beach", 
+//      price:1000,
+//      location:"kushinagar",
+//      country:"India",
+//    });
+//    await sampleListing.save();
+//    console.log("sample was saved");
+//    res.send("successful testing");
    
-})
+// })
 app.listen(8080,()=>{
      console.log("Server is listing");
      
