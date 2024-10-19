6cfdf85c911041e6b11d0fa8dbb946877ab075eb
Rishabh <smartrishabh51@gmail.com>
2024-10-19 17:44:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get("/listings/:id/edit", async (req,res)=>{
 app.put("/listings/:id",async (req,res)=>{
      let {id} = req.params;
    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-   res.redirect("/listings");
+   res.redirect(`/listings/${id}`);
 });
 // app.get("/testlisting",async(req,res)=>{
 //    let sampleListing = new Listing({
