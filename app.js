0c33248397cd63da9f20027326a9566eca1f4e59
Rishabh <smartrishabh51@gmail.com>
2024-10-19 10:56:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const express = require("express")
 const app = express();
+const mongoose = require("mongoose");
+main().then(()=>{
+     console.log("connceted to db");    
+})
+.catch(err =>{
+     console.log(err);
+     
+});
+async function main(){
+     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+app.get("/",(req,res)=>{
+     res.send("Hi,I a root");
+     
+})
+app.listen(8080,()=>{
+     console.log("Server is listing");
+     
+});
